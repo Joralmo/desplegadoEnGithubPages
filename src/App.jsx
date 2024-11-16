0a99f95842a804e11ede9da4e57d0e97b61a7527
Jose Rafael Altamar Molina <JoralmoPro@Gmail.com>
2024-11-16 11:16:39 -0500

    Revert "Añadiendo parrafo"
    This reverts commit a317007ab269fc46d633703305927ee5d96149a2.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
       <h1>Mi Gran Proyecto</h1>
       <div className="card">
         <h1>Proyecto desplegado en github pages</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
       </div>
     </>
   );
